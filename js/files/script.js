ccad64dff06d07d4bc3accf3357a36367de912b2
antizyk <01antizyk.it@gmail.com>
2021-03-15 09:54:01 +0200

    Багованый слайдер

--- js/files/script.js
+++ js/files/script.js
@@ -42,16 +42,24 @@ function slider() {
         prev = document.querySelector('.products__arrow_p'),
         next = document.querySelector('.products__arrow_n'),
         slider = document.querySelector('.slider'),
-        slidesField = document.querySelector('.slider__wrap'),
         indicators = document.querySelector('.products__dots'),
         dots = [],
+//---------------------------Медиа запросы--------------------------------------
+        md1 = window.matchMedia('(min-width: 991px)'),
+        md2 = window.matchMedia('(max-width: 991px)'),
+        md3 = window.matchMedia('(min-width: 479px)'),
+        md4 = window.matchMedia('(max-width: 479px)'),
+//---------------------------/Медиа запросы--------------------------------------
         width = window.getComputedStyle(slider).width,
         numWith = Math.round(+width.slice(0, width.length - 2));
-
+    let slidesField = document.querySelector('.slider__wrap');
     let arrItems = [];
     let slideIndex = 1;
     let indent = 0;
 
+
+
+    //-------------------Добавляет классы точкам----------
     function addClassNeighbour(arrDots) {
         arrDots.forEach((dot, index) => {
             if (index == slideIndex - 1) {
@@ -71,6 +79,8 @@ function slider() {
             }
         });
     }
+    //-------------------//Добавляет классы точкам----------
+    //-------------------Cоздает блок обертку----------
     function addWrapBlok(list, nameClass, quantity) {
         list.forEach((el, index, arr) => {
             if (arrItems.length < quantity) {
@@ -95,29 +105,82 @@ function slider() {
             }
         })
     }
-    /* function unwrap(wrapper) {
-        let docFrag = document.createDocumentFragment();
-        while (wrapper.firstChild) {
-            let child = wrapper.removeChild(wrapper.firstChild);
-            docFrag.appendChild(child);
+
+    //================EXPEREMENT=============================
+    function breakPoint(e) {
+        if (e.matches) {
+            document.querySelectorAll('.slider__block').forEach(el => {
+                var fragment = document.createDocumentFragment();
+                while (el.firstChild) {
+                    fragment.appendChild(el.firstChild);
+                }
+                el.parentNode.replaceChild(fragment, el);
+            })
+            console.log('md1');
+            addWrapBlok(subslides, 'slider__block', 4);
+            let slides = document.querySelectorAll('.slider__block')
+            slides.forEach(slide => {
+                slide.style.width = width;
+            })
+            slidesField.style.width = 100 * slides.length + '%'; //Ширина обертки слайдера
+            slides.forEach(slide => {
+                slide.style.width = width;
+            })
+           /*  console.log(slidesField.childNodes);
+            slidesField.childNodes.forEach(el => {
+                if (el.nodeName == 'DIV') {
+                    console.log(el);
+                    if (el.classList.contains('slider__item')) {
+                        el.remove();
+                    }
+                }
+            }) */
+            return slides;
         }
+    }
+
+
+    function breakPointTwo(e) {
+        console.log(e.matches);
+        if (e.matches) {
+            
+            document.querySelectorAll('.slider__block').forEach(el => {
+                var fragment = document.createDocumentFragment();
+                while (el.firstChild) {
+                    fragment.appendChild(el.firstChild);
+                }
+                el.parentNode.replaceChild(fragment, el);
+            })
+            console.log('md2');
+            addWrapBlok(subslides, 'slider__block', 3);
+            let slides = document.querySelectorAll('.slider__block')
+            slides.forEach(slide => {
+                slide.style.width = width;
+            })
+            slidesField.style.width = 100 * slides.length + '%'; //Ширина обертки слайдера
+            //-------------------Ширина слайда------------------
+            slides.forEach(slide => {
+                slide.style.width = width;
+            })
+            //-------------------/Ширина слайда------------------
+            /* console.log(slidesField.childNodes);
+            slidesField.childNodes.forEach(el => {
+                if (el.nodeName == 'DIV') {
+                    console.log(el);
+                    if (el.classList.contains('slider__item')) {
+                        el.remove();
+                    }
+                }
+            }) */
+            return slides;
+        }
+    }
+    md2.addListener(breakPointTwo);
+    md1.addListener(breakPoint);
+    breakPointTwo(md2);
+    breakPoint(md1);
+    //================EXPEREMENT=============================
     
-        wrapper.parentNode.replaceChild(docFrag, wrapper);
-    } */
-    function unwrap(node) {
-        node.replaceWith(...node.childNodes);
-    }
-
-    if (document.documentElement.clientWidth > 991.98) {
-        addWrapBlok(subslides, 'slider__block', 4);
-    } else if (document.documentElement.clientWidth < 991.98 && document.documentElement.clientWidth > 767.98) {
-        addWrapBlok(subslides, 'slider__block', 3);
-    } else if (document.documentElement.clientWidth < 767.98 && document.documentElement.clientWidth > 479.98) {
-        addWrapBlok(subslides, 'slider__block', 2);
-    } else if (document.documentElement.clientWidth < 479.98) {
-        addWrapBlok(subslides, 'slider__block', 1);
-    }
-
 
     subslides.forEach(el => {
         if (!el.parentElement.classList.contains('slider__block')) {
@@ -125,38 +188,15 @@ function slider() {
         }
     })
 
-    const slides = document.querySelectorAll('.slider__block')
+    let slides = document.querySelectorAll('.slider__block')
+    //slides.forEach(el => el.style.width = width);
 
 
-    /* window.addEventListener('resize', () => {
-        //console.log(document.documentElement.clientWidth);
-        if (document.documentElement.clientWidth > 991) {
-            console.log('h1');
-            slides.forEach(el => {
-                unwrap(el);
-            })
-            addWrapBlok(subslides, 'slider__block', 4);
-        } else if (document.documentElement.clientWidth == 990) {
-            slides.forEach(el => {
-                unwrap(el);
-            })
-            addWrapBlok(subslides, 'slider__block', 3);
-        } else if (document.documentElement.clientWidth == 767) {
-            slides.forEach(el => {
-                unwrap(el);
-            })
-            addWrapBlok(subslides, 'slider__block', 2);
-        } else if (document.documentElement.clientWidth == 479) {
-            slides.forEach(el => {
-                unwrap(el);
-            })
-            addWrapBlok(subslides, 'slider__block', 1);
-        }
-    }) */
-    slidesField.style.width = 100 * slides.length + '%';
+    
+  /*   slidesField.style.width = 100 * slides.length + '%';
     slides.forEach(slide => {
         slide.style.width = width;
-    })
+    }) */
 
     for (let i = 0; i < slides.length; i++) {
         const dot = document.createElement('span');
@@ -172,6 +212,7 @@ function slider() {
         indicators.append(dot);
         dots.push(dot);
     }
+    //------------------------Настройка элементов навигации------------------
     next.addEventListener('click', () => {
         if (indent === numWith * (slides.length - 1)) {
             indent = 0;
@@ -204,12 +245,28 @@ function slider() {
             addClassNeighbour(dots);
         })
     })
+    //------------------------/Настройка элементов навигации------------------
+
+
 
 }
 
 //=========================/SLIDER======================
 spoiler();
 slider();
+
+    //-------------------/Cоздает блок обертку----------
+    /* function unwrap(wrapper) {
+        let docFrag = document.createDocumentFragment();
+        while (wrapper.firstChild) {
+            let child = wrapper.removeChild(wrapper.firstChild);
+            docFrag.appendChild(child);
+        }
+    
+        wrapper.parentNode.replaceChild(docFrag, wrapper);
+    } */
+
+
 /* fetch('/index.html')
     .then(function (response) {
         return response.text()
@@ -217,3 +274,52 @@ slider();
         document.querySelector('.slider').innerHTML = body
     })
  */
+
+/* const mediaQuery = window.matchMedia('(min-width: 768px)')
+function handleTabletChange(e) {
+    if (e.matches) {
+        console.log('Media Query Matched!')
+    }
+}
+mediaQuery.addListener(handleTabletChange)
+handleTabletChange(mediaQuery) */
+
+/*  window.addEventListener('resize', () => {
+         console.log(document.documentElement.clientWidth);
+         if (document.documentElement.clientWidth == 992) {
+             console.log('h1');
+             fetch('/index.html')
+                 .then(function (response) {
+                     return response.text()
+                 }).then(function (body) {
+                     document.querySelector('.slider__wrap').innerHTML = body
+                 }).then(console.log('Exelent'))
+             addWrapBlok(subslides, 'slider__block', 4);
+         } else if (document.documentElement.clientWidth == 990) {
+             slides.forEach(el => {
+                 unwrap(el);
+             })
+             addWrapBlok(subslides, 'slider__block', 3);
+         } else if (document.documentElement.clientWidth == 767) {
+             slides.forEach(el => {
+                 unwrap(el);
+             })
+             addWrapBlok(subslides, 'slider__block', 2);
+         } else if (document.documentElement.clientWidth == 479) {
+             slides.forEach(el => {
+                 unwrap(el);
+             })
+             addWrapBlok(subslides, 'slider__block', 1);
+         }
+     }) */
+
+/*     if (window.matchMedia("(min-width: 991px)").matches) {
+       addWrapBlok(subslides, 'slider__block', 4);
+   } else if (window.matchMedia("(min-width: 767px)").matches) {
+       addWrapBlok(subslides, 'slider__block', 3);
+   } else if (window.matchMedia("(min-width: 479px)").matches) {
+       addWrapBlok(subslides, 'slider__block', 2);
+   } else if (window.matchMedia("(max-width: 479px)").matches) {
+       addWrapBlok(subslides, 'slider__block', 1);
+   }
+*/
